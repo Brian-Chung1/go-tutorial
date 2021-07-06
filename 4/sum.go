4d3eba2e39fbe1ac469d8a2b34c144ab6f90dd6d
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-07-05 22:55:13 -0700

    go go go

--- 4/sum.go
+++ 4/sum.go
@@ -1,9 +1,46 @@
 package main
 
-func Sum(numbers [5]int) int {	
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	return
+}
+
+//Ways to iterate over array
+// i := 0
+// for i <= 3 {
+//     fmt.Println(i)
+//     i = i + 1
+// }
+
+// for i := 0; j <= 3; i++ {
+//     fmt.Println(j)
+// }
+
+// for {
+//     fmt.Println("loop")
+//     break
+// }
+
+// for n := 0; n <= 5; n++ {
+//     if n%2 == 0 {
+//         continue
+//     }
+//     fmt.Println(n)
+// }
+
+// range gives back two values: index, value
+// for index, value := range numbers {
+//		fmt.Println(value)
+// }
+
+//(_ blank identifer is used to ignore a value and in this case we ignore the index)
+// for _, number := range numbers {
+//		fmt.Println(number)
+// }
--- 4/sum_test.go
+++ 4/sum_test.go
@@ -1,24 +1,51 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
 
-	assertCorrectMessage := func(t testing.TB, got, want int, numbers [5]int) {
+	assertCorrectMessage := func(t testing.TB, got, want int, numbers []int) {
 		t.Helper()
 		if got != want {
 			t.Errorf("got %d want %d given %v", got, want, numbers)
 		}
 	}
 
-	t.Run("sums numbers in array", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
 		assertCorrectMessage(t, got, want, numbers)
 	})
 
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+		assertCorrectMessage(t, got, want, numbers)
+	})
+
+}
+
+func TestSumAll(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("sum of two slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+    want := []int{3, 9}
+
+    if got != want {
+        t.Errorf("got %v want %v", got, want)
+    }
+	})
 }
