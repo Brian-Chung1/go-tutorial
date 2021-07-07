a708223739d5e11a235577d8beba255d804e306e
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-07-06 22:13:02 -0700

    go go go go

--- 4/sum.go
+++ 4/sum.go
@@ -8,8 +8,56 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	return
+//Alternate SumAll
+// func SumAll(numbersToSum ...[]int) []int {
+// 	lengthOfNumbers := len(numbersToSum)
+// 	sums := make([]int, lengthOfNumbers)
+
+// 	for i, numbers := range numbersToSum {
+// 		sums[i] = Sum(numbers)
+// 	}
+
+// 	return sums
+// }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+//Alternate SumAllTails
+// func SumAllTails(numbersToSum ...[]int) []int {
+// 	var sums []int
+
+// 	for _, numbers := range numbersToSum {
+// 		currSum := 0
+// 		for i := 1; i < len(numbers); i++ {
+// 			currSum += numbers[i]
+// 		}
+// 		sums = append(sums, currSum)
+// 	}
+
+// 	return sums
+// }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
 
 //Ways to iterate over array
--- 4/sum_test.go
+++ 4/sum_test.go
@@ -42,10 +42,32 @@ func TestSumAll(t *testing.T) {
 
 	t.Run("sum of two slices", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
-    want := []int{3, 9}
-
-    if got != want {
-        t.Errorf("got %v want %v", got, want)
-    }
+		want := []int{3, 9}
+		assertCorrectMessage(t, got, want)
 	})
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("sum of two slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 10})
+		want := []int{5, 19}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+
+	})
+}
+
+//https://blog.golang.org/slices-intro
